86d3d74a0864e1caf8715590d1733f0830109249
Manish Chanda <youremail@domain.com>
2021-09-04 11:36:10 +0530

    .

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,12 +1,12 @@
 const Student = require("../models/student");
 
 exports.saveDetails = (req, res) => {
-  console.log(req.body.name);
-  console.log(req.body.class);
-  console.log(req.body.course);
+  console.log(req.body);
+  // console.log(req.body.class);
+  // console.log(req.body.course);
 
-  console.log(req.body.roll_no);
-  console.log(req.body.contact_no);
+  // console.log(req.body.roll_no);
+  // console.log(req.body.contact_no);
   let newStudent = new Student(req.body);
   newStudent.save((err, data) => {
     if (err) {
